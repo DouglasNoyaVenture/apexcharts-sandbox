2fc1e81eaa5f07ce88b554c61368b96469365350
DouglasNoyaVenture <douglas@noyaventures.com>
2024-09-17 13:56:45 -0600

    feature(charts) | New leads Chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { CurrentLeads } from './Charts/CurrentLeads/CurrentLeads';
+import { NewLeads } from './Charts/NewLeads/NewLeads';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <CurrentLeads />
+      <NewLeads />
     </main>
   );
 }
